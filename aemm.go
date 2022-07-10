1b71bbc235c7da9170e3e62c22f27014e03b3cbb
purpleclay <purpleclaygh@gmail.com>
2022-07-10 10:54:09 +0100

    add comments

--- aemm.go
+++ aemm.go
@@ -57,16 +57,48 @@ func Container(ctx context.Context) (testcontainers.Container, error) {
 	return ContainerWith(ctx, DefaultOptions)
 }
 
-// LaunchOptions ...
+// LaunchOptions defines all configurable options when launching the AEMM container
 type LaunchOptions struct {
-	// StrictIMDSv2 ...
+	// StrictIMDSv2 will enforce IMDSv2 and require a session token when making metadata
+	// requests. A token is requested by issuing a PUT request to the token endpoint, and
+	// supplying a TTL of between 1 and 2600 seconds.
+	//
+	//	PUT localhost:1338/latest/api/token -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"
+	//
+	// Any subsequent request must provide the token as a header:
+	//
+	// 	GET localhost:1338/latest/metadata/local-ipv4 -H "X-aws-ec2-metadata-token: $TOKEN"
 	StrictIMDSv2 bool
 }
 
-// DefaultOptions ...
+// DefaultOptions provides a way to launch the AEMM container with a strict set of defaults
 var DefaultOptions = LaunchOptions{}
 
-// ContainerWith ...
+// ContainerWith will create and start an instance of the Amazon EC2 Metadata Mock (AEMM),
+// simulating the Amazon EC2 Metadata Service (IMDS). The launch behaviour of the AEMM
+// can be configured through the provided LaunchOptions. Once started, IMDS will
+// be accessible through the following endpoint:
+//
+// http://localhost:1338/latest/metadata
+//
+// By using the default settings, both IMDSv1 and IMDSv2 are supported. Metadata about the
+// mocked EC2 instance can then be retrieved using any of the documented categories,
+// https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instancedata-data-categories.html
+//
+// For example:
+// 	curl http://localhost:1338/latest/metadata/local-ipv4
+// 	curl http://localhost:1338/latest/metadata/block-device-mapping/root
+//
+// To ensure your AWS config is configured to call this mock, the required option needs to
+// be set:
+//
+// 	package main
+//
+//	import "github.com/aws/aws-sdk-go-v2/config"
+//
+//	func main() {
+//		config.LoadDefaultConfig(context.TODO(), config.WithEC2IMDSEndpoint("http://localhost:1338/latest/metadata"))
+//	}
 func ContainerWith(ctx context.Context, opts LaunchOptions) (testcontainers.Container, error) {
 	flags := []string{}
 	if opts.StrictIMDSv2 {
